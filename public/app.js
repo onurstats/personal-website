116714559f78125cc2d03c38270db4136867b675
Onur Yanar <onur4185@google.com>
2024-02-22 14:55:48 +0300

    json ld

--- public/app.js
+++ public/app.js
@@ -1,24 +1,28 @@
 
 const app = () => {
-  const logo = document.getElementById("hero-logo");
 
-  const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true,
-  });
 
-  scroll.on("scroll", (e) => {
-    const delta = e.delta.y;
-    const scaleX = (300 - (300 - delta)) / 300;
-    const scale = 0.7 - scaleX;
+  let lastKnownScrollPosition = 0;
+  let ticking = false;
 
-    logo.style.position = "fixed";
-    logo.style.transform = `translateX(-${delta * 3}px)`;
-    if (scale > 0.3) {
-      logo.style.scale = scale;
+  function doSomething(scrollPos) {
+    // Do something with the scroll position
+    console.log(scrollPos);
+  }
 
+  document.addEventListener("scroll", (event) => {
+    lastKnownScrollPosition = window.scrollY;
+
+    if (!ticking) {
+      window.requestAnimationFrame(() => {
+        doSomething(lastKnownScrollPosition);
+        ticking = false;
+      });
+
+      ticking = true;
     }
   });
+
 }
 
 app();
